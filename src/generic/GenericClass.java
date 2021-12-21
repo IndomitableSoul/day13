49ab2df8cafbadc5dd9900afbce14f71881d75d8
nikita <amarnikita09@gmail.com>
2021-12-21 13:28:28 +0530

    refactored GenericClass.java to UC5

--- src/generic/GenericClass.java
+++ src/generic/GenericClass.java
@@ -1,6 +1,6 @@
 package generic;
 
-/*
+/* Refactor-2
 Re-factored to create Generic
 Class to take in 3 variables of
 Generic Type*/
@@ -16,20 +16,22 @@ public class GenericClass<E extends Comparable>{
 	 }
 	
 	public <E extends Comparable> void max() {
-		maxGeneric(a, b, c);
+		printMax(maxGeneric(a, b, c));
 	 }
 	
-	public static <E extends Comparable> void maxGeneric(E a, E b, E c) {
+	public static <E extends Comparable> E maxGeneric(E a, E b, E c) {
 		E max;
 		if(a.compareTo(b)>0 && a.compareTo(c)>0) 
 			 max = a;
 		else if(b.compareTo(c)>0)
 			 max = b;
 	    else
-			 max = c;
-		System.out.println("Maximum is: " + max);   
+			 max = c; 
+		return max;
 	  }
-	
+	public <E> void printMax(E max) {
+		System.out.println("Maximum is: " + max);
+	}
 	public static void main(String[] args) {
 	   new GenericClass<Integer>(10, 20, 30).max();
 	   new GenericClass<Double>(1.0, 2.3, 3.4).max();
