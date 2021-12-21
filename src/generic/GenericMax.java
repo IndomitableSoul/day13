0a5887ab3dd911276bc149ad256911507fb5a9d5
nikita <amarnikita09@gmail.com>
2021-12-21 12:13:22 +0530

    Refactored GenericMax.java to find max of float values

--- src/generic/GenericMax.java
+++ src/generic/GenericMax.java
@@ -25,6 +25,18 @@ public class GenericMax{
 		   System.out.println(c + " is max");
 	   
     }
+   
+ //UC-1: Find max of 3 Strings	
+   public static void max(String a, String b, String c) {
+	   
+	   if(a.compareTo(b)>0 && a.compareTo(c)>0)
+		   System.out.println(a  +" is max");
+	   else if(b.compareTo(c)>0)
+		   System.out.println(b + " is max");
+	   else
+		   System.out.println(c + " is max");
+	   
+    }
 	
    public static void main(String[] args) {
 	System.out.println("_____________");
@@ -35,6 +47,10 @@ public class GenericMax{
 	max(3.3f, 1.4f, 0.5f);
 	max(3.3f, 4.4f, 0.5f);
 	max(3.3f, 4.4f, 5.5f);
+	System.out.println("_____________");
+	max("Welcome", "Hello", "World");
+	max("Welcome", "World", "Hello");
+	max("World", "Welcome", "Hello");
 	System.out.println("_____________");
 }
 }
