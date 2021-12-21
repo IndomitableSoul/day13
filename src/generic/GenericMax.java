97ee9b5242d3ffae2a26bfd62d2e75404ffddf70
nikita <amarnikita09@gmail.com>
2021-12-21 12:02:39 +0530

    Refactored GenericMax.java to all scenaios of UC1

--- src/generic/GenericMax.java
+++ src/generic/GenericMax.java
@@ -2,7 +2,7 @@ package generic;
 
 public class GenericMax{
 	
-   //UC-1: Find max of 3 Intergers	
+   //UC-1: Find max of 3 Integers	
    public static void max(Integer a, Integer b, Integer c) {
 	   
 	   if(a.compareTo(b)>0 && a.compareTo(c)>0)
@@ -15,7 +15,9 @@ public class GenericMax{
    }
 	
    public static void main(String[] args) {
-	max(3,14,5);
+	max(33, 14, 5);
+	max(33, 44, 5);
+	max(33, 44, 55);
 	
 }
 }
